6288e8eb15f031d17454a774a56efa4d17156bee
João Victor Morais <86614027+jvmoraiscb@users.noreply.github.com>
2022-04-13 15:08:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const rwClient = require("./twitterClient.js");
 
 const tweet = async() => {
     try {
-        await rwClient.v1.tweet("Vasco!");
+        await rwClient.v1.tweet("Au au! (Vasco!)");
     } catch(e) {
         console.error(e)
     }
